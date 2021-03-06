269dd8d540c619371200e3d7bdddddc7ce68ad13
Yury Vlasik <vlasik@adv.ru>
2021-03-06 12:07:59 +0300

    added CoroutineCallAdapterFactory

--- app/src/main/java/com/example/mvvmgithubsampleapp/di/AppModule.kt
+++ app/src/main/java/com/example/mvvmgithubsampleapp/di/AppModule.kt
@@ -1,11 +1,11 @@
 package com.example.mvvmgithubsampleapp.di
 
 import com.example.mvvmgithubsampleapp.repository.ProjectService
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
 import dagger.Module
 import dagger.Provides
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.converter.moshi.MoshiConverterFactory
 import javax.inject.Singleton
 
 @Module
@@ -16,7 +16,7 @@ internal class AppModule {
         return Retrofit.Builder()
             .baseUrl("https://api.github.com/")
             .addConverterFactory(GsonConverterFactory.create())
-            .addConverterFactory(MoshiConverterFactory.create())
+            .addCallAdapterFactory(CoroutineCallAdapterFactory())
             .build()
             .create(ProjectService::class.java)
     }
